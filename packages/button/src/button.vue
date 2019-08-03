ac0335968c710ef55cf5b80f2c3182713df500b2
linwens_xdf <linwenshu@xdf.cn>
2019-08-03 17:26:09 +0800

    修改按钮样式，scss语法及BEM规范还要加强

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,52 +1,42 @@
 <template>
-    <button
-            class='xu-button'
-            :class="[`icon-${iconPosition}`, type ? 'xu-button--' + type : '', disabled ? 'xu-button--disabled': '']"
-            @click="$emit('click')"
-            :disabled="disabled">
-        <xu-icon class='icon' v-if='icon && !loading' :name='icon'></xu-icon>
-        <xu-icon class='loading icon' v-if='loading' name='loading'></xu-icon>
-        <div class='xu-button-content'>
-            <slot></slot>
-        </div>
-    </button>
+  <button
+    @click="$emit('click')"
+    class="xu-button"
+    :disabled="disabled"
+    :class="[
+    'xu-button--'+type,
+    {'is-plain': plain, 'is-disabled': disabled, 'is-round': round},
+    'xu-button--size-'+size
+    ]"
+  >
+    <i v-if="icon !== ''" :class="icon"></i>
+    <slot></slot>
+  </button>
 </template>
 <script>
-    import XuIcon from '../../icon/src/icon.vue'
-
-    export default {
-        name: 'XuButton',
-        props: {
-            type: {
-                type: String,
-                defalut: 'success',
-                validator(value) {
-                    if (value) {
-                        const types = ['primary', 'success', 'info', 'warning', 'danger']
-                        if(!types.includes(value)) throw new Error('type is not exist')
-                    }
-                    return value
-                }
-            },
-            icon: {},
-            loading: {
-                type: Boolean,
-                default: false
-            },
-            disabled: {
-                type: Boolean,
-                default: false
-            },
-            iconPosition: {
-                type: String,
-                default: 'left',
-                validator(value) {
-                    return !(value !== 'left' && value !== 'right')
-                }
-            },
-        },
-        components: {
-            XuIcon
-        }
+  export default{
+    name: 'XuButton',
+    props: {
+      type: {
+        type: String,
+        default: 'default'
+      },
+      size: {
+        type: String,
+        default: 'default'
+      },
+      icon: {
+        type: String,
+        default: ''
+      },
+      plain: Boolean,
+      disabled: Boolean,
+      round: Boolean
+    },
+    data () {
+      return {
+        msg: 'button'
+      }
     }
+  }
 </script>
