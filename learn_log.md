af8f3a205d568171ab9cf0d923ab4f04616c54ec
linwens_xdf <linwenshu@xdf.cn>
2019-08-08 00:43:26 +0800

    karma自动化测试，travis集成测试研究中

--- learn_log.md
+++ learn_log.md
@@ -8,7 +8,8 @@
 - ` md.scss ` 需要本地化一下
 - ` packages.json ` 执行脚本集成
 - ` CHANGELOG ` 增加更新日志
-- ` unit test ` 单元测试编写
+- ` unit test ` 单元测试编写 ` mocha `, ` chai `, ` sinon `
+- ` karma ` 做自动化测试
 - ` scss ` ` bem ` ` postcss-salad `编写规范及写法
 - ` theme-default ` 有没有办法加一个.scss就自动往index.scss写入
 - ` .sh ` 文件写一个，知道是干嘛的
--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -8,8 +8,8 @@ chai.use(sinonChai)
 describe('Button', () => {
 
   it('存在', () => {
-    const wrapper = mount(Button)
-    expect(wrapper.html()).to.include('<button class="xu-button xu-button--default xu-button--default"><!----><!----><span></span></button>')
+    // eslint-disable-next-line no-unused-expressions
+    expect(Button).to.exist
   })
   // 能否设置icon
   it('可以设置icon', () => {
@@ -20,5 +20,76 @@ describe('Button', () => {
     })
     const el = wrapper.find('i')
     expect(el.attributes()['class']).to.equal('xu-icon-taobao')
+  })
+  // 设置按钮类型
+  it('type', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        type: 'primary'
+      }
+    })
+    const el = wrapper.find('button')
+    expect(el.attributes()['class']).to.include('xu-button--primary')
+  })
+  // 设置按钮size
+  it('size', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        size: 'mini'
+      }
+    })
+    const el = wrapper.find('button')
+    expect(el.attributes()['class']).to.include('xu-button--mini')
+  })
+  // 设置按钮disabled
+  it('disabled', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        disabled: true
+      }
+    })
+    const el = wrapper.find('button')
+    expect(el.attributes()['class']).to.include('is-disabled')
+  })
+  // 设置按钮圆角round
+  it('round', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        round: true
+      }
+    })
+    const el = wrapper.find('button')
+    expect(el.attributes()['class']).to.include('is-round')
+  })
+  // 设置朴素按钮
+  it('plain', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        plain: true
+      }
+    })
+    const el = wrapper.find('button')
+    expect(el.attributes()['class']).to.include('is-plain')
+  })
+  // 设置loading
+  it('可以设置loading', () => {
+    const wrapper = mount(Button, {
+      propsData: {
+        loading: true
+      }
+    })
+    const el = wrapper.find('i')
+    expect(el.attributes()['class']).to.equal('xu-icon-loading')
+  })
+  // 可以触发click事件
+  it('可以触发click事件', () => {
+    const wrapper = mount(Button)
+    const vm = wrapper.vm
+
+    const cb = sinon.fake()
+    vm.$on('click', cb)
+    vm.$el.click()
+    // eslint-disable-next-line no-unused-expressions
+    expect(cb).to.have.been.called
   })
 })
