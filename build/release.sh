38b78aabd2ad482c754845049b2b60ad84da5187
linwens <lws200841001@gmail.com>
2019-08-25 23:39:59 +0800

    ci(travis): 修改release为输入确定

--- build/release.sh
+++ build/release.sh
@@ -1,13 +1,14 @@
-# 生成组件入口文件
-npm run build:entry
-# 获取最后一次提交的commit -m
-COMMIT_M = `git log --pretty=format:"%s" -1`
-# 打包dist，并发布到gh-page分支上，实现展示页面的更新，(下面涉及的变量在travis网站上配置)
-npm run build
-cd ./dist
-git init
-git config user.name "$U_NAME"
-git config user.email "$U_EMAIL"
-git add .
-git commit -m "$COMMIT_M"
-git push --force --quiet "https://$GH_TOKEN@$GH_REF" master:$P_BRANCH
+
+set -e # 如果任何语句的执行结果不是true则应该退出
+echo "Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # standard-verson修改version
+  npm run version -m "[release]: $VERSION" -r $VERSION
+  # 发版
+  git push origin master $VERSION
+fi
