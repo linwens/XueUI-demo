625d733edeb1e346bd74d3510254676d3bf70059
linwens_xdf <linwenshu@xdf.cn>
2019-08-26 10:20:04 +0800

    ci(travis): gh-page增加提交message

--- build/deploy-ci.sh
+++ build/deploy-ci.sh
@@ -1,5 +1,5 @@
 # 用于发布npm包
-
+set -e
 # 清理已有的 lib
 npm run clean
 # 跳转到 packages/theme-default 安装依赖
--- build/deploy-ghpage.sh
+++ build/deploy-ghpage.sh
@@ -1,7 +1,8 @@
+set -e
 # 生成组件入口文件
 npm run build:entry
 # 获取最后一次提交的commit -m
-COMMIT_M = `git log --pretty=format:"%s" -1`
+COMMIT_M=`git log --pretty=format:"%s" -1`
 # 打包dist，并发布到gh-page分支上，实现展示页面的更新，(下面涉及的变量在travis网站上配置)
 npm run build
 cd ./dist
